502286e16791cafacd48467c32c7422d19a8808b
JihunCha <jhcha@rsupport.com>
2022-02-25 15:21:11 +0900

    openCameraCallback 까지 설정..

--- app/src/main/java/com/example/facedetecionsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/facedetecionsampleapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.facedetecionsampleapp
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.facedetecionsampleapp.databinding.ActivityMainBinding
 import com.example.facedetecionsampleapp.ui.DetectionActivity
 
 class MainActivity : AppCompatActivity() {
     companion object {
         val TAG : String = MainActivity::class.java.simpleName
+        private const val CAMERA_REQUEST_CODE = 1000
     }
 
     private lateinit var binding : ActivityMainBinding
@@ -22,10 +28,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
+        Log.d(TAG, "initView")
+
         binding.btSample1.setOnClickListener {
-            val intent = Intent(this, DetectionActivity::class.java)
-            startActivity(intent)
+            val cameraPermissionCheck = ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            )
+
+            if (cameraPermissionCheck != PackageManager.PERMISSION_GRANTED) { // 권한이 없는 경우
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    CAMERA_REQUEST_CODE
+                )
+            } else {
+                //권한이 있는 경우
+                startDetectionActivity()
+            }
         }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                //권한이 거부된 경우
+                Toast.makeText(this@MainActivity, "No Permission!!", Toast.LENGTH_SHORT).show()
+            } else {
+                //승인이 된 경우
+                startDetectionActivity()
+            }
+        }
+    }
+
+    private fun startDetectionActivity() {
+        Log.d(TAG, "startDetectionActivity()")
+        val intent = Intent(this, DetectionActivity::class.java)
+        startActivity(intent)
+    }
 }
